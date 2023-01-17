38cf0d1ec81c14a2b6607bfd22c431441c60b853
michael.danko@passportinc.com <michael.danko@passportinc.com>
2023-01-17 13:16:16 -0500

    Modified 18 files: Began defining `curb-event` modal
    Committed by omf-service-account on behalf of michael.danko@passportinc.com

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE1Mjc1Nw/spectral.js?branch=v1.0.0-dev&token=6e92d70f-60d0-452c-94a9-f25bafb5f760";
+export default { extends: ruleset };
